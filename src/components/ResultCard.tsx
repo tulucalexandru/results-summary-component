96558c937daaec1a816bf23bc2ea783d3142d214
Alexandru Tuluc <alexandrutuluc@Alexandrus-Laptop.local>
2024-10-03 15:04:15 +0200

    9th commit

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/ResultCard.module.scss'
 const ResultCard = () => {
     return (
         <Card className={styles.card}>
-            <h1>Your result</h1>
+            <h1>Your Result</h1>
         <Result></Result>
         <h2>Great</h2>
         <p>Your performance exceed 65% of the people
